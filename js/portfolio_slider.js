6f0bc65ea51663e7ba9fed74e1788f9a4bb638c6
Kristian Relloma <kristianrelloma23@yahoo.com>
2017-09-05 18:22:11 +0800

    Advertising-PumpPro

---
+++ js/portfolio_slider.js
@@ -0,0 +1,145 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({	
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+			{
+			'title' : 'Pump Pro Cement Grouting & Spurting Machine Features:',
+			'description'   : 'It is designed with tungsten steel pump valves to assure of the service expectancy with longer life cycle. <br>It is functioned with automatic timing operation with the constant volume of lubricating system availably.<br>The conveying operation can be operated by LCD panel control or remote control.<br>The conveying speed can be adjusted in accordance with the actual demand availably ranging from 0 to maximum grouting volume.',
+			
+			
+			'thumbnail' : ['img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/1.jpg', 'img/portfolio/large/1.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Cement Grouting','All']
+			},
+
+			{
+			'title' : 'Pump Pro Cement Grouting & Spurting Machine Features:',
+			'description'   : '<iframe width="560" height="315" src="https://www.youtube.com/embed/EMyhs9Y0678" frameborder="0" allowfullscreen></iframe><br>It is designed with tungsten steel pump valves to assure of the service expectancy with longer life cycle. <br>It is functioned with automatic timing operation with the constant volume of lubricating system availably.<br>The conveying operation can be operated by LCD panel control or remote control.<br>The conveying speed can be adjusted in accordance with the actual demand availably ranging from 0 to maximum grouting volume.',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/2.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/2.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Cement Grouting ']
+			},
+
+			
+
+			{
+			'title' : 'Hydraulic Oscillator',
+			'description'   : '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/CWoWWg6-_4Q" frameborder="0" allowfullscreen></iframe><br>It is designed with hi-pressure screen of hydraulic pump output to protect the oil hydraulic system and elongate the service expectancy. <br>It is designed with thoroughly automatic operational functions with constant lubricating system to re-lubricate each joint pin periodically so that it can reduce the operation burden of operator.<br>It is designed with level measuring devices and the clamping indicators of upper and lower pipeline clamps.',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/3.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/3.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Hydraulic Oscillator']
+			},
+			
+			{
+				'title' : 'Trailer Concrete Pump',
+			'description'   : 'Powerful patented concrete pumps<br>With "Deutz" diesel air cooling engine, it is efficient and low noisy.',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/4.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/4.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Trailer Concrete ']
+			},
+
+			{
+		'title' : 'Concrete Pump Trucks Without Arm (Pipefitting Carry)',
+			'description'   : 'CF pump vale with wolfram steel: It is made by wolfram with special extra work to get the good quality and wonderful functions, and it is awarded from Germany, China and Japan...etc.<br> The design of the main oil tub: It divides into oil and water. When the oil seal got break, the tub will separate the oil and water directly.<br> Injection lubrication system for pump valve shaft and reamer shaft adopts automatic instant-volume control which decreased the waste of grease',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/5.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/5.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Truck']
+			},
+			
+			{
+			'title' : '32Meter Concrete Pump Truck',
+			'description'   : 'CF pump vale with wolfram steel: It is made by wolfram with special extra work to get the good quality and wonderful functions, and it is awarded from Germany, China and Japan...etc.<br> The design of the main oil tub: It divides into oil and water. When the oil seal got break, the tub will separate the oil and water directly.<br> Injection lubrication system for pump valve shaft and reamer shaft adopts automatic instant-volume control which decreased the waste of grease.',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/6.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/6.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Truck']
+			},
+
+			{
+			'title' : '23Meter Concrete Pump Truck',
+			'description'   : 'CF pump vale with wolfram steel: It is made by wolfram with special extra work to get the good quality and wonderful functions, and it is awarded from Germany, China and Japan...etc.<br> The design of the main oil tub: It divides into oil and water. When the oil seal got break, the tub will separate the oil and water directly.<br> Injection lubrication system for pump valve shaft and reamer shaft adopts automatic instant-volume control which decreased the waste of grease.',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/7.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/7.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Truck']
+			},
+
+			
+
+			{
+			'title' : '37Meter Concrete Pump Truck',
+			'description'   : '<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/xek1lOO9dDU" frameborder="0" allowfullscreen></iframe><br>CF pump vale with wolfram steel: It is made by wolfram with special extra work to get the good quality and wonderful functions, and it is awarded from Germany, China and Japan...etc.<br> The design of the main oil tub: It divides into oil and water. When the oil seal got break, the tub will separate the oil and water directly.<br> Injection lubrication system for pump valve shaft and reamer shaft adopts automatic instant-volume control which decreased the waste of grease.',
+			
+			
+			
+			'thumbnail' : ['img/portfolio/small/8.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg', 'img/portfolio/small/1.jpg'],
+			'large' : ['img/portfolio/large/8.jpg', 'img/portfolio/large/2.jpg', 'i', ''],
+			'button_list'   :
+			[
+			
+			
+			],
+			'tags'  : ['Truck']
+			},
+
+			
+	 
+		]
+	});
+});
